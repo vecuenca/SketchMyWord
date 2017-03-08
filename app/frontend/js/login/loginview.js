e2d14d7e7d55cd4197bc0af1784b25c92cfea5f7
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2017-03-08 17:13:44 -0500

    add login view

---
+++ app/frontend/js/login/loginview.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+var view = (function() {
+  "use strict";
+
+  var view = {};
+
+	document.getElementById('signin').onsubmit = function(e) {
+		e.preventDefault();
+		
+		var username = document.getElementById('signin-username').value;
+		var password = document.getElementById('signup-password').value;
+
+		if (username.length > 0 && password.length > 0) {
+			var event = new CustomEvent('onSignIn', {
+				detail: {
+					username: username,
+					password: password
+				}
+			});
+			document.dispatchEvent(event);
+			document.getElementById('signin').reset();
+			console.log(event);
+		} else {
+			view.displayError('Please enter a username or password');
+		}
+	}
+
+	view.displayError = function(errorMsg) {
+		document.getElementById('error').innerHTML = errorMsg;
+	}
+
+  return view;
+
+}());
