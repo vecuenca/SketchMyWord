285346610cc5b153d51a9bfa6c06860b39fdd07b
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2017-03-12 10:07:20 -0400

    remove commented socket code

--- app/server/socket.js
+++ app/server/socket.js
@@ -1,28 +1,6 @@
 module.exports = {
 	roomHandler: function (io, rooms) {
 		io.on('connection', function (socket) {
-			// // should be fired when we redirect to index.html
-			// socket.on('init_user', function (username, room) {
-
-			// 	// verify user actually belongs to this room
-			// 	if (!rooms[room].users.contains(username)) {
-			// 		return; // broadcast unauthorized message?
-			// 	}
-
-			// 	// set socket data to use later
-			// 	socket.username = username;
-			// 	socket.room = room;
-
-			// 	socket.join(room);
-
-			// 	// send line history so far
-			// 	var lineHistory = rooms[room].lineHistory;
-			// 	for (var line in lineHistory) {
-			// 		socket.broadcast.to(room)
-			// 			.emit('draw_line', { line: lineHistory[line] });
-			// 	}
-			// });
-
 			// has the client's socket join the requested room
 			socket.on('join_room', function (username, room) {
 				console.log('cur room state', rooms);
