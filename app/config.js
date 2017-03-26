00a7457a8e6603c783a8992bc7abc8c71eef37ae
Jason Zheng <yongzhez@github.com>
2017-03-26 13:56:57 -0400

    changing to localhost

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "directory": __dirname,
     "logLocation": __dirname + "/logs/importantinformation.log",
     "mysql": {
-        "host" : 'cmsvm23.utsc.utoronto.ca',
+        "host" : 'localhost',
         "user" : 'root',
         "password" : '1234',
         "database" : 'sketch-my-word',
